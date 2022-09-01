d83a1e19f534426442db0bea5035737283e3cc1e
Alvaro <ahmamani@ucsp.edu.pe>
2022-09-01 08:18:39 -0500

    Programa sumar enteros

---
+++ semana2/sumandoEnteros.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    a = 20;
+
+    int b;
+    b = 10;
+
+    int suma = a + b;
+
+    cout << "La suma es : " << suma << endl;
+
+    return 0;
+}
