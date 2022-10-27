3822a4a361423f7563875933833f101f07c6b8f2
Alvaro <ahmamani@ucsp.edu.pe>
2022-10-27 08:29:49 -0500

    clases y punteros - static intarray

---
+++ semana10/ArregloEstatico/IntArray.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "IntArray.h"
+
+IntArray::IntArray(int size) {
+    data = new int[size];
+    this->size = size;
+}
+
+IntArray::IntArray(const IntArray &o) {
+    size = o.size;
+    data = o.data;
+}
+
+void IntArray::setAt(int index, int value) {
+    if(index >= 0 && index < size)
+        data[index] = value;
+}
+
+int IntArray::getAt(int index) const {
+    if(index >= 0 && index < size)
+        return data[index];            
+}
+
+int IntArray::getSize() const {
+    return size;
+}
+
+void IntArray::print() const {
+    for(int i = 0; i < size; i++)
+        std::cout << data[i] << " ";
+    std::cout << std::endl;
+}
+
+IntArray::~IntArray() {
+    delete [] data;
+}
